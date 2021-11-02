9150316804795a7dd3844456a1286ba2d6d7f2e1
Breanne <BreMarotta@gmail.com>
2021-11-02 18:57:33 -0400

    commit tree 4ce895855b716af4cd45e10d02a2a4c30db2d8ba

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun(activity= "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work= "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(flare= "*"){
+    return function (parameter= "special"){
+        return `You are ${flare}${parameter}${flare}!`
+    }
+}
